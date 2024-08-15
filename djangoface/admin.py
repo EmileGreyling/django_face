30f383b757f11cf8dfeef8d3fe92e34d3096fa21
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-08-15 08:24:28 +0200

    Remove unwanted files

---
+++ djangoface/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Face, User
+
+admin.site.register(Face)
+admin.site.register(User)
---
+++ djangoface/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+
+    # Authentication routes
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+]
