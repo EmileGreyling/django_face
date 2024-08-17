0fc1eaad9e7925753502ed38bc177b24279926ba
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-08-17 19:14:15 +0200

    Add modal

--- capstone/settings.py
+++ capstone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d3=uu%$g3!_*%fy8y&afk9ht%%!^brco&61m7z9s_)*hcqyy2p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- djangoface/static/djangoface/app.js
+++ djangoface/static/djangoface/app.js
@@ -27,9 +27,18 @@ let form = $("#image-form").submit(function (e) {
             // Display image
             var img_base64 = response.image;
             var img = document.createElement("img");
-            img.height = "500";
+            img.class = "img-fluid";
+            img.style.width = "100%";
             img.src = "data:image/jpeg;base64," + img_base64;
+            
+            // Add image to screen
             $("#image-container").html(img);
+
+            $("#download-btn").attr("download", "results.jpeg");
+            $("#download-btn").attr("target", "_blank");
+            $("#download-btn").attr("href", "data:image/jpeg;base64," + img_base64);
+
+            $("#imageModal").modal('show');
         },
         error: function (xhr, textStatus, error) {
             // Handle error response
